2b2d817b69d6277c5153a6cf645295f3ef164ff6
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2021-03-10 10:41:35 +0100

    IIFE for the frameworks own execution context. It protects my variables

---
+++ plum-vigilant.js
@@ -0,0 +1,7 @@
+// Create a new execution context for our entire framework, 
+// so that all our variables declared are safe and we are only exposing on the global object what we want.
+
+( function( global, $) {
+
+    
+}(global, jQuery) );
